2f63280820bef312342a3fb6e88e78922b3d42f4
Laurent Bassin <laurent@bassin.info>
2018-09-30 12:27:00 +0200

    Fix bundle error

--- config/bundles.php
+++ config/bundles.php
@@ -6,6 +6,7 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Overblog\GraphQLBundle\OverblogGraphQLBundle::class => ['all' => true],
-    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    // Dev only
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['dev' => true],
     Overblog\GraphiQLBundle\OverblogGraphiQLBundle::class => ['dev' => true],
 ];
