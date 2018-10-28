9b3079652945a5b4b5851c170af08f5232290968
Laurent Bassin <laurent@bassin.info>
2018-10-28 10:43:27 +0100

    Fix get form functional test

--- tests/GraphQL/Resolver/Container/FormWebTest.php
+++ tests/GraphQL/Resolver/Container/FormWebTest.php
@@ -2,27 +2,36 @@
 
 namespace App\Tests\GraphQL\Resolver\Container;
 
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Bundle\FrameworkBundle\Client;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 /**
  * @author Laurent Bassin <laurent@bassin.info>
  */
 class FormWebTest extends WebTestCase
 {
+    /** @var Client $client */
+    private $client;
+
+    protected function setUp()
+    {
+        $this->client = static::createClient();
+        static::$container->get('cache.app')->clear();
+    }
+
     public function testListAllContainersForm()
     {
         /** @var Client $client */
-        $client = static::createClient();
+        $this->client = static::createClient();
 
-        $client->request(
+        $this->client->request(
             'POST',
             '/graphql/',
             ['query' => '{container_form(id: "nginx"){id}}']);
 
-        $containerForms = json_decode($client->getResponse()->getContent(), true);
+        $containerForms = json_decode($this->client->getResponse()->getContent(), true);
 
-        $this->assertSame(200, $client->getResponse()->getStatusCode());
+        $this->assertSame(200, $this->client->getResponse()->getStatusCode());
         $this->assertEmpty($containerForms['errors'] ?? [], 'Errors entry not empty');
         $this->assertNotEmpty($containerForms['data']['container_form'] ?? [], 'No container found');
     }
