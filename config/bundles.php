5a38887e6ac9fedb0b1dfa1748230fcd15acceb7
Laurent Bassin <laurent@bassin.info>
2018-11-01 18:15:06 +0100

    Remove cloud upload

--- config/bundles.php
+++ config/bundles.php
@@ -4,5 +4,4 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
-    Knp\Bundle\GaufretteBundle\KnpGaufretteBundle::class => ['all' => true],
 ];
