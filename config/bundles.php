faa4f2d885bf4ab5cf4f97c85fd3502973e93f6c
Laurent Bassin <laurent@bassin.info>
2019-01-26 15:13:32 +0100

    Update monolog

--- config/bundles.php
+++ config/bundles.php
@@ -2,9 +2,9 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Aws\Symfony\AwsBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
