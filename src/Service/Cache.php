68a339a46d1bfe239ef6d3411e90eb8b55f50881
Laurent Bassin <laurent@bassin.info>
2019-04-10 22:39:21 +0200

    Add cache for github files

---
+++ src/Service/Cache.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service;
+
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Cache\Adapter\AbstractAdapter;
+
+/**
+ * @author Laurent Bassin <laurent@bassin.info>
+ */
+class Cache
+{
+    private $adapter;
+    private $logger;
+
+    public function __construct(AbstractAdapter $adapter, LoggerInterface $logger)
+    {
+        $this->adapter = $adapter;
+        $this->logger = $logger;
+    }
+
+    public function has(string $key): bool
+    {
+        $key = $this->normalizeKey($key);
+
+        $item = $this->adapter->getItem($key);
+
+        return $item->isHit();
+    }
+
+    public function get(string $key)
+    {
+        $key = $this->normalizeKey($key);
+
+        $item = $this->adapter->getItem($key);
+        if (!$item->isHit()) {
+            return null;
+        }
+
+        return $item->get();
+    }
+
+    public function set(string $key, $data): void
+    {
+        $key = $this->normalizeKey($key);
+
+        $item = $this->adapter->getItem($key);
+        $item->set($data);
+        $item->expiresAt(null);
+
+        $this->adapter->save($item);
+    }
+
+    private function normalizeKey(string $key): string
+    {
+        $key = str_replace('/', '.', $key);
+
+        return $key;
+    }
+}
