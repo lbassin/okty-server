7b45d7850d1fba5bdb76c50450b6d9caa5c712f6
w-raken <maxime.marquet1@gmail.com>
2019-06-12 09:41:07 +0200

    change size of results

--- src/Controller/Api/Registry/Search.php
+++ src/Controller/Api/Registry/Search.php
@@ -31,7 +31,7 @@ class Search
     public function handle(Request $request): JsonResponse
     {
         $search = $request->query->get('query');
-        $response = $this->httpClient->request('GET', "https://hub.docker.com/v2/search/repositories/?query=$search");
+        $response = $this->httpClient->request('GET', "https://hub.docker.com/v2/search/repositories/?query=$search&page_size=25");
 
         return new JsonResponse(
             $this->serializer->serialize($response->getContent(), 'json'),
