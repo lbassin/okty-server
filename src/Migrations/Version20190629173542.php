5a683eaea71537f39e9316e5a7f6474f41fb688d
Laurent Bassin <laurent@bassin.info>
2019-06-29 19:36:31 +0200

    Add migration

--- src/Migrations/Version20190629173542.php
+++ src/Migrations/Version20190629173542.php
@@ -7,16 +7,8 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
-final class Version20190612190952 extends AbstractMigration
+final class Version20190629173542 extends AbstractMigration
 {
-    public function getDescription() : string
-    {
-        return '';
-    }
-
     public function up(Schema $schema) : void
     {
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
