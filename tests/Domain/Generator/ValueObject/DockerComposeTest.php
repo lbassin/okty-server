59cf6330af3c1d1b73eded49205ece9c13ee2d54
Laurent Bassin <laurent@bassin.info>
2019-10-22 22:43:26 -0400

    Add docker-compose version tests

---
+++ tests/Domain/Generator/ValueObject/DockerComposeTest.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Domain\Generator\ValueObject;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @author Laurent Bassin <laurent@bassin.info>
+ */
+class DockerComposeTest extends TestCase
+{
+    public function test_dummy(): void
+    {
+        $this->doesNotPerformAssertions();
+    }
+}
---
+++ tests/Domain/Generator/ValueObject/VersionTest.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Domain\Generator\ValueObject;
+
+use App\Domain\Generator\Exception\DockerCompose\UnknownDockerComposeVersionException;
+use App\Domain\Generator\ValueObject\DockerCompose\Version;
+use PHPUnit\Framework\TestCase;
+
+class VersionTest extends TestCase
+{
+    /**
+     * @dataProvider wrongVersionProvider
+     */
+    public function test_exception_thrown_if_wrong_version_value(string $version): void
+    {
+        $this->expectException(UnknownDockerComposeVersionException::class);
+
+        new Version($version);
+    }
+
+    /**
+     * @dataProvider availableVersionProvider
+     */
+    public function test_existing_version_are_allowed(string $value): void
+    {
+        $version = new Version($value);
+
+        $this->assertInstanceOf(Version::class, $version);
+    }
+
+    public function wrongVersionProvider(): array
+    {
+        return [
+            ['WrongVersion'],
+            [''],
+            [4],
+            ['Won\'t work'],
+            ['0.14'],
+        ];
+    }
+
+    public function availableVersionProvider(): array
+    {
+        $reflectionClass = new \ReflectionClass(Version::class);
+
+        return array_map(function ($version) {
+            return [$version];
+        }, $reflectionClass->getConstant('AVAILABLE_VERSIONS'));
+    }
+}
